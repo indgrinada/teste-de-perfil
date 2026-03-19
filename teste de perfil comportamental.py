093e5f677e231b78d2039af85a46a20f49c4af38
indgri <ingridgmartins04@gmail.com>
2026-03-18 23:49:12 -0300

    programa em python ver. 2
    segunda versão funcional do programa: agora ele está um pouco mais organizado em funções, possui um dicionário para armazenar as respostas e conta com um menu de opções ("Teste", "Resultados Gerais" e "Sair" )

--- teste de perfil comportamental.py
+++ teste de perfil comportamental.py
@@ -1,31 +1,64 @@
-"""
-PERFIS
-A - Líder
-B - Pesquisador
-C - Executor 
-D - Revisor 
+# PERFIS: A - Líder, B - Pesquisador, C - Executor, D - Revisor
 
-"""
-resposta_bruta = "aceito críticas, mas nao fico calada"
-resposta_padrao = []
-opcoes = {"A": 0, "B": 0, "C": 0, "D": 0}
+# Início - define as variáveis necessárias
+menu = "T"
+perfis = {}
+    
 
-print("TESTE DE PERFIL COMPORTAMENTAL")
-print("Responda as perguntas com as opções A, B, C ou D. Digite uma resposta por linha, ou o digito 'F' para finalizar:")
-while resposta_bruta != "F":
-     resposta_bruta = input().upper()
-     resposta_padrao.append(resposta_bruta)
+# Teste - recebe as respostas, computa e apresenta o resultado
+def teste(perfis):
+    n = 1
+    resposta_bruta = "aceito críticas, mas nao fico calada"
+    resposta_padrao = []
+    opcoes = {"A": 0, "B": 0, "C": 0, "D": 0}
 
-# Contagem 
-for linha in resposta_padrao:
-   for char in linha:
-       if char in opcoes:
-           opcoes[char] += 1
+    # Entrada - recebe e padroniza as respostas, até o respondente digitar "F" para finalizar
+    print("\nTESTE DE PERFIL COMPORTAMENTAL\n")
+    nome = input("Nome do respondente: ").upper()
+    print("Digite as respostas dadas, uma em cada linha, ou \"F\" para finalizar:")
+    while resposta_bruta != "F":
+        resposta_bruta = input(f"{n:2}: ").upper()
+        resposta_padrao.append(resposta_bruta)
+        n += 1
 
-n = opcoes["A"] + opcoes["B"] + opcoes["C"] + opcoes["D"]
+    # Contagem - conta quantas vezes cada opção (A, B, C, D) aparece nas respostas padronizadas
+    for linha in resposta_padrao:
+        for char in linha:
+            if char in opcoes:
+                opcoes[char] += 1
 
-# Saída
-print("RESULTADO DO TESTE:")
-for perfil, percil in opcoes.items():
-    print(f"{perfil}: {(percil/n)*100:.2f}%")
-    print(opcoes["A"])
+    total = opcoes["A"] + opcoes["B"] + opcoes["C"] + opcoes["D"]
+    perfis[nome] = {
+        "Líder": opcoes["A"] / total * 100,
+        "Pesquisador": opcoes["B"] / total * 100,
+        "Executor": opcoes["C"] / total * 100,
+        "Revisor": opcoes["D"] / total * 100
+    }
+
+    # Saída - exibe o resultado, em porcentagem, para cada face de perfil comportamental
+    print("\nRESULTADO DO TESTE\n")
+    for face, porcentagem in perfis[nome].items():
+        print(f"{face}: {porcentagem:.2f}%")
+
+
+# Visualização geral - exibe os resultados de todos os respondentes, em porcentagem, para cada face de perfil comportamental
+def visual(perfis):
+    print("\nRESULTADOS GERAIS")
+    for nome in perfis.keys():
+        print(f"\n{nome}: ")
+        for face, porcentagem in perfis[nome].items():
+            print(f"{face}: {porcentagem:.2f}%")
+
+
+# Menu - permite, ao respondente, escolher entre realizar outro teste, visualizar os resultados ou sair do programa
+while menu != "S":
+    if menu == "T":
+        teste(perfis)
+        menu = input("\n\nDigite \"T\" para realizar outro teste, \"V\" para visualizar os resultados, ou \"S\" para sair do programa: ").upper()
+    elif menu == "V":
+        visual(perfis)
+        menu = input("\n\nDigite \"T\" para realizar outro teste, \"V\" para visualizar os resultados, ou \"S\" para sair do programa: ").upper()
+    else:
+        menu = input("Opção inválida. Digite \"T\" para realizar outro teste, \"V\" para visualizar os resultados, ou \"S\" para sair do programa: ").upper()
+
+print("\n\nFim do programa.")
